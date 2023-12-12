6a7766c61391471c86db1931492d3df376a5e87c
heriwahyudiono <heriwhydiono@gmail.com>
2023-12-12 07:52:03 +0700

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <WebView
+      source={{ uri: '' }}
+    />
+  );
+};
+
+export default App;
+
+
+
